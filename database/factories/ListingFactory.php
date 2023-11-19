6c007a09144c8d3a13d1546bbad4baaf4389bc33
victor <2236843@swansea.ac.uk>
2023-11-19 20:08:19 +0000

    Creation of web layout  -- (after adding variables to my listing)

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+        'title' => fake()->sentence(),
+        'tags' => fake()->randomElement(['laravel', 'api', 'backend']),
+        'company' => fake()->companyEmail(),
+        'location' => fake()->city(),
+        'email' => fake()->email(),
+        'website' => fake()->url(),
+        'description' => fake()->paragraph(5),
+        ];
+    }
+}
